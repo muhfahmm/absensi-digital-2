51347cd4e30a1bac809d3a93cb005192ab5bffae
Muhfahmm <fahimfahim0407@gmail.com>
2026-06-25 17:03:44 +0700

    Add perubahan 1

--- md/migrations/add_qrcode_columns.sql
+++
@@ -1,3 +0,0 @@
--- Migration: add qrcode column to existing tables
-ALTER TABLE tb_guru ADD COLUMN IF NOT EXISTS qrcode VARCHAR(255) NULL UNIQUE;
-ALTER TABLE tb_siswa ADD COLUMN IF NOT EXISTS qrcode VARCHAR(255) NULL UNIQUE;
